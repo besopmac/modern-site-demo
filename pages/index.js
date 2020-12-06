1688d203b059df9b442b41fa40b43db0d5fc3f7d
Bruno N. Campos <bruno@mambowifi.com>
2020-12-06 12:11:11 -0300

    primeira pagina

---
+++ pages/index.js
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+
+const Home = () => {
+  return (
+    <>
+      <h1>Contador</h1>
+      <Contador />
+    </>
+  )
+}
+
+const Contador = () => {
+  const [contador, setContador] = useState(1);
+
+  function adicionarContador() {
+    setContador(contador + 1);    
+  }
+
+  return (
+    <div>
+      <h2>{contador}</h2>
+      <button onClick={adicionarContador}>Adiciona +1</button>
+    </div>
+  )
+}
+
+export default Home
